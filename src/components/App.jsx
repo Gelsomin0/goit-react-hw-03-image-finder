92befca07c2fe087c1bfbb1b7c1fae616ba86032
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-07-30 17:36:54 +0300

    Some changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,32 @@ import css from './App.module.css';
 import { getSearchData } from 'tools/getSearchData';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { FallingLines } from 'react-loader-spinner';
+import { Button } from './Button/Button';
 
 export class App extends Component {
   state = {
     searchQuery: '',
     searchData: [],
     page: 1,
+    isLoadedGallery: true,
+    canLoadMore: false,
   }
 
   onSubmit = (newQuery) => {
-    this.setState({ searchQuery: newQuery });
+    if (!newQuery) {
+      return;
+    }
+
+    this.setState({
+      searchQuery: newQuery,
+      searchData: [],
+      page: 1,
+      isLoadedGallery: false,      
+    });
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     if (this.state.searchQuery === '') {
       return;
     }
@@ -23,11 +36,33 @@ export class App extends Component {
     if (prevState.searchQuery !== this.state.searchQuery) {
       getSearchData(this.state.searchQuery, this.state.page)
         .then(res => res.json())
-        .then((res) => {
-          this.setState({ searchData: [...res.hits] });
+        .then(({hits, total}) => {
+          this.setState({ searchData: [...hits], isLoadedGallery: true });
+
+          if (total > 12) this.setState((prevState) => {
+            return {
+              canLoadMore: true,
+              page: prevState.page + 1,
+            }
+          });
         })  
       return;
     }
+  }
+
+  onLoadMore = () => {
+    getSearchData(this.state.searchQuery, this.state.page)
+      .then(res => res.json())
+      .then(({ hits }) => {
+        if (hits.length < 12) this.setState({ canLoadMore: false });
+        
+        this.setState((prevState) => {
+          return {
+            searchData: [...prevState.searchData, ...hits],
+            page: prevState.page + 1,
+          }
+        })
+      })
   }
   
   render() {
@@ -37,7 +72,19 @@ export class App extends Component {
           onSubmit={this.onSubmit}
         />
 
+        {!this.state.isLoadedGallery && 
+          <div className={css.loader}>
+            <FallingLines
+              color="#4fa94d"
+              width="100"
+              visible={true}
+              ariaLabel='falling-lines-loading'
+            />
+          </div>          
+        }      
+
         {this.state.searchData.length > 0 && <ImageGallery collection={this.state.searchData} />}
+        {this.state.canLoadMore && <Button onLoadMore={this.onLoadMore} />}
 
       </div>
     );
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import css from './Button.module.css';
+
+export const Button = ({onLoadMore}) => {
+    return (
+        <button
+            className={css.button}
+            onClick={onLoadMore}
+        >Load More</button>
+    );
+}
